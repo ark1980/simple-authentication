88fd340e6acab2ba1e346f210d7759df94f69ce6
Alireza Keshanian <ar-keshanian@hotmail.com>
2015-10-19 21:18:13 +0000

    Modified users signup view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      flash.notice = "Signed up!"
-      redirect_to "/"
+     redirect_to root_url, :notice => "Signed up!"
     else
       render :new
     end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index' 
   resources :posts
+  resources :users
   get "sign_up" => "users#new", :as => "sign_up"
 end
